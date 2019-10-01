ee8e95c9ed12cf0c32742aa7b94f05d800ce48ad
digger <digger@mysmf.net>
2019-10-01 14:13:09 +0400

    Add Russian localization

---
+++ languages/ThemeStrings.russian.php
@@ -0,0 +1,12 @@
+<?php
+
+$txt['mycss'] = 'Добавить свои стили CSS';
+$txt['mycss_desc'] = '
+';
+$txt['a_unread'] = 'Новые сообщения';
+$txt['a_replies'] = 'Новые ответы';
+$txt['a_ownposts'] = 'Мои сообщения';
+$txt['a_options'] = 'Настройки';
+$txt['admin_toggle'] = 'Админка';
+
+?>
